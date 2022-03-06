e74ab0f9c87e28513a357a234a85db5926b1f8da
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-06 09:58:53 +0600

    console.log after 3.5s using setTimeout() method

---
+++ setTimeout.js
@@ -0,0 +1,6 @@
+/* 
+    ১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে। ,
+*/
+setTimeout( () => {
+    console.log('Result will show after 3.5s');
+}, 3500);
